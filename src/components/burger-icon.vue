cff427886de4a52e02ae7d0b2d5a1e9b4e83c870
skiwqv <andreus0212@gmail.com>
2023-10-26 20:43:10 +0300

    icon-change

---
+++ src/components/burger-icon.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="icons">
+      <svg
+        width="50"
+        height="50"
+        viewBox="0 0 32 42"
+        xmlns="http://www.w3.org/2000/svg"
+        onclick="this.classList.toggle('active')"
+      >
+      </svg>
+      <svg
+        width="50"
+        height="50"
+        viewBox="0 0 32 42"
+        xmlns="http://www.w3.org/2000/svg"
+        onclick="this.classList.toggle('active')"
+      >
+        <g transform="matrix(1,0,0,1,-438.286,-264.004)">
+          <g id="arrow_left1">
+            <g
+              transform="matrix(-1,-1.22465e-16,1.22465e-16,-1,858.787,564.935)"
+            >
+              <path
+                id="top"
+                d="M390,270L420,270L420,270C420,270 420.195,250.19 405,265C389.805,279.81 390,279.967 390,279.967"
+              />
+            </g>
+            <g transform="matrix(-1,-2.44929e-16,-2.44929e-16,1,858.786,5)">
+              <path
+                id="bottom"
+                d="M390,270L420,270L420,270C420,270 420.195,250.19 405,265C389.805,279.81 390,279.967 390,279.967"
+              />
+            </g>
+            <g
+              transform="matrix(-1,-1.22465e-16,1.22465e-16,-1,858.787,569.935)"
+            >
+              <path id="middle" d="M390,284.967L420,284.967" />
+            </g>
+          </g>
+        </g>
+      </svg>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+svg {
+    fill-rule: evenodd;
+    clip-rule: evenodd;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-miterlimit: 1.5;
+    cursor:pointer;
+  }
+
+  svg path {
+    fill: none;
+    stroke: white;
+    stroke-width: 1px;
+  }
+
+  #top,
+  #bottom {
+    stroke-dasharray: 30, 75.39;
+    transition: all 0.6s cubic-bezier(0.6, 0.33, 0.67, 1.29);
+  }
+
+  svg.active #top,
+  svg.active #bottom {
+    stroke-dasharray: 75.39;
+    stroke-dashoffset: -60;
+  }
+
+  svg:nth-child(2) {
+    transform: rotate(0deg);
+    transition: transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  }
+
+  svg:nth-child(2) path {
+    transition: all 0.4s ease-in-out 0.6s;
+  }
+
+  svg:nth-child(2).active {
+    transform: rotate(180deg);
+  }
+
+  .rocket #top {
+    stroke-dasharray: 30, 88;
+  }
+</style>
--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -15,16 +15,18 @@
         <router-link :to="link.to" active-class="navbar-active" class="navbar__link">{{ link.name }}</router-link>
       </section>
     </vue-horizontal>
-    <input  type="checkbox" @click="openMenu" role="button" aria-label="Display the menu" class="menu">
+    <!-- <input  type="checkbox" @click="openMenu" role="button" aria-label="Display the menu" class="menu"> -->
+    <burger-icon class="menu" @click="openMenu"></burger-icon>    
     <div v-if="isOpen" class="menu_fixed">
       <router-link  v-for="link in links" :key="link" :to="link.to" active-class="menu-active" class="menu__link">{{ link.name }}</router-link>
-    </div>    
+    </div>
   </div>
 </template>
 
 <script setup>
 import {ref } from "vue";
 import VueHorizontal from "vue-horizontal";
+import burgerIcon from "@/components/burger-icon.vue"
 
 const links = ref([{
   name: 'GAGA',
@@ -71,74 +73,13 @@ function openMenu() {
   width: 40%;
 }
 
-.menu {
-  --s: 30px;
-  /* control the size */
-  --c: white;
-  /* the color */
-  display: none;
-
-  height: var(--s);
-  aspect-ratio: 1;
-  opacity: 0.7;
-  border: none;
-  padding: 0;
-  border-inline: calc(var(--s)/2) solid #0000;
-  box-sizing: content-box;
-  --_g1: linear-gradient(var(--c) 20%, #0000 0 80%, var(--c) 0) no-repeat content-box border-box;
-  --_g2: radial-gradient(circle closest-side at 50% 12.5%, var(--c) 95%, #0000) repeat-y content-box border-box;
-  background:
-    var(--_g2) left var(--_p, 0px) top,
-    var(--_g1) left calc(var(--s)/10 + var(--_p, 0px)) top,
-    var(--_g2) right var(--_p, 0px) top,
-    var(--_g1) right calc(var(--s)/10 + var(--_p, 0px)) top;
-  background-size:
-    20% 80%,
-    40% 100%;
-  position: relative;
-  clip-path: inset(0 25%);
-  -webkit-mask: linear-gradient(90deg, #0000, #000 25% 75%, #0000);
-  cursor: pointer;
-  transition:
-    background-position .3s var(--_s, .3s),
-    clip-path 0s var(--_s, .6s);
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-}
-
-.menu:before,
-.menu:after {
-  content: "";
+.menu{
   position: absolute;
-  border-radius: var(--s);
-  inset: 40% 0;
-  background: var(--c);
-  transition: transform .3s calc(.3s - var(--_s, .3s));
+  top: 20px;
+  width: 30px;
+  height: 30px;
+  display: none;
 }
-
-.menu:checked {
-  clip-path: inset(0);
-  --_p: calc(-1*var(--s));
-  --_s: 0s;
-}
-
-.menu:checked:before {
-  transform: rotate(45deg);
-}
-
-.menu:checked:after {
-  transform: rotate(-45deg);
-}
-
-.menu:focus-visible {
-  clip-path: none;
-  -webkit-mask: none;
-  border: none;
-  outline: 2px solid var(--c);
-  outline-offset: 5px;
-}
-
 .replaced-btn {
   background-color: white;
   border-radius: 90%;
@@ -210,8 +151,10 @@ function openMenu() {
   .menu {
     display: block;
     position: absolute;
+    top:-50px;
+    opacity: 1;
     z-index: 999;
-    right: 30px;
+    right: 45px;
   }
   .menu_fixed {
     display: block;
